4ae4f1acb2ad9cf440b6aafd3d88d5668c09d691
Kathlyn Miller <katkit101999@hotmail.com>
2015-01-28 22:32:36 -0800

    watched video 12 coded and commented

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -13,7 +13,7 @@
         }]);
         
         this.body.setVelocity(5, 20); // setting velocity to its number and changed 0 to 20
-        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH); // helpful/useful for our player entity
 
         this.renderable.addAnimation("idle", [78]);
         this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80); // using images for animation
@@ -66,7 +66,7 @@
             this.body.onCollision = this.onCollision.bind(this);
             this.type = "PlayerBaseEntity";
 
-            this.renderable.addAnimation("idle", [0]);
+            this.renderable.addAnimation("idle", [0]);  // added animation for base
             this.renderable.addAnimation("broken", [1]);
             this.renderable.setCurrentAnimation("idle");
 
@@ -106,7 +106,7 @@
             this.body.onCollision = this.onCollision.bind(this);
             this.type = "EnemyBaseEntity";
 
-            this.renderable.addAnimation("idle", [0]);
+            this.renderable.addAnimation("idle", [0]); // added animation for base
             this.renderable.addAnimation("broken", [1]);
             this.renderable.setCurrentAnimation("idle");
  		},

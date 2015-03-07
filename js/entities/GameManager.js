704dd694429ffad6b4c5321ea4705d5fbdf2ab5e
Kathlyn Miller <katkit101999@hotmail.com>
2015-03-06 17:44:26 -0800

    watched video 37 coded and commented

---
+++ js/entities/GameManager.js
@@ -0,0 +1,71 @@
+ game.GameTimerManager = Object.extend({ // added game man
+      init: function(x, y, settings){
+          this.now = new Date().getTime();
+          this.lastCreep = new  Date().getTime();
+          this.paused = false;
+          this.alwaysUpdate = true;
+      },
+
+      update: function(){
+      	  this.now = new Date().getTime();
+          this.goldTimerCheck();
+          this.creepTimerCheck();
+      	
+          return true;
+      },
+
+      goldTimerCheck: function(){
+
+          if(Math.round(this.now/1000)%20 ===0 && (this.now - this.lastCreep >= 1000)){
+      	  	  game.data.gold += 1;
+      	  	  console.log("Current gold: " + game.data.gold);
+      	  }
+
+      },
+
+      creepTimerCheck: function(){
+
+      	   if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
+      	  	this.lastCreep = this.now;
+      	  	var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
+      	  	me.game.world.addChild(creepe, 5);
+
+
+      	  }
+      }
+  });
+ 
+game.HeroDeathManager = Object.extend({ // made a hero death manager
+	init: function(x, y, settings){
+        this.alwaysUpdate = true;
+	},
+
+	update: function(){
+          if(game.data.player.dead){ 
+      	      me.game.world.removeChild(game.data.player); //for my player to reset when dead
+      	  	  me.state.current().resetPlayer(10, 0)
+      	  }
+	}
+});
+
+game.ExperienceManager = Object.extend({ //making experience manager 
+	init: function(x, y ,settings){
+		this.alwaysUpdate = true;
+		this.gameOver = false;
+
+	},
+
+	update: function(){
+		if(game.data.win === true && !this.gameOver){ // game over for game
+            game.data.exp += 10;
+            this.gameOver = true;
+		}else if(game.data.win === false && !this.gameOver){
+            game.data.exp += 1;
+            this.gameOver = true;
+		} 
+		console.log(game.data.exp);
+
+        return true;
+	}
+
+});

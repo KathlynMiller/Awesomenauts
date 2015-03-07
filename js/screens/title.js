4d774339bdace4e4b2aef0699f3c84da659e5c84
Kathlyn Miller <katkit101999@hotmail.com>
2015-03-06 18:57:23 -0800

    watched video 39 coded and commented

---
+++ js/screens/title.js
@@ -0,0 +1,45 @@
+game.TitleScreen = me.ScreenObject.extend({
+	/**	
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {	
+		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO // added new image
+
+	
+		me.game.world.addChild(new (me.Renderable.extend({
+            init: function(){
+                this._super(me.Renderable, 'init', [270, 240, 300, 50]);
+                this.font = new me.Font("Arial", 46, "white"); //play around with fonts
+                me.input.registerPointEvent('pointerdown', this, this.newGame.bind(this), true);
+            },
+
+            draw: function(renderer){
+            	this.font.draw(renderer.getContext(), "START A NEW GAME", this.pos.x, this.pos.y); // passing the context of where we are
+            	
+            },
+
+            update: function(dt){
+                  return true;
+            },
+
+            newGame: function(){
+                  me.input.releasePointEvent('pointerdown', this); // used in newGame function
+                  me.save.remove('exp');
+                  me.save.remove('exp1');
+                  me.save.remove('exp2');
+                  me.save.remove('exp3');
+                  me.save.remove('exp4');
+                  me.state.remove(me.state.PLAY);
+            }
+        })));
+
+	},
+	
+	
+	/**	
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		
+	}
+});

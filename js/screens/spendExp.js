e722a3d2b0de0c8b2ff37de2837537141709aac5
Kathlyn Miller <katkit101999@hotmail.com>
2015-03-13 11:23:15 -0700

    wartched 43 commented video

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -1,1 +1,36 @@
-spendExp.js
+game.SpendExp = me.ScreenObject.extend({
+	/**	
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {	
+	     	me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('exp-screen')), -10); // TODO // added new image
+
+	
+		    me.game.world.addChild(new (me.Renderable.extend({
+            init: function(){
+                this._super(me.Renderable, 'init', [10, 10, 300, 50]); // changed to 10
+                this.font = new me.Font("Arial", 46, "white"); //play around with fonts
+                
+            },
+
+            draw: function(renderer){
+            	this.font.draw(renderer.getContext(), "PRESS F1-F4 TO BUY, F5 TO SKIP", this.pos.x, this.pos.y); // passing the context of where it is
+              this.font.draw(renderer.getContext(), "CURRENT EXP: " + game.data.exp.toString(), this.pos.x + 100 , this.pos.y + 50); // making font for a title on a screen 
+              this.font.draw(renderer.getContext(), "F1: INCREASE GOLD PRODUCTION " + game.data.exp.toString(), this.pos.x + 100 , this.pos.y + 100); // passing the context of where it is
+              this.font.draw(renderer.getContext(), "F2: ADD STARTING GOLD " + game.data.exp.toString(), this.pos.x + 100 , this.pos.y + 150); // passing the context of where it is
+              this.font.draw(renderer.getContext(), "F3: INCREASE ATTACK DAMAGE " + game.data.exp.toString(), this.pos.x + 100 , this.pos.y + 200); // passing the context of where it is
+              this.font.draw(renderer.getContext(), "F4: INCREASE STARTING HEALTH " + game.data.exp.toString(), this.pos.x + 100 , this.pos.y + 250); // passing the context of where it is
+            	
+            }
+
+        })));
+	},
+	
+	
+	/**	
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		
+	}
+});
